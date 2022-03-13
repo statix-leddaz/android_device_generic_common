bdef556e06a76118f82ec13c628bee5f3da89673
Tom Pratt <tom.pratt@outlook.com>
2022-03-13 10:35:26 +0100

    Compile guard aosp apns conf in gsi product
    Conflicts with StatiX's BUILD_PREBUILT apns and can't be fixed by include order.
    
    Change-Id: Idb7dca607243fefffe8c16de019a75f26d275ddf

--- gsi_product.mk
+++ gsi_product.mk
@@ -34,5 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound?=pixiedust.ogg \
     ro.com.android.dataroaming?=true \
 
+ifeq ($(STATIX_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
